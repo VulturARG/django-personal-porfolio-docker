3ebeef139bd41dd2ee952525b545735f02aafbf8
VulturARG <luisbriones@gmail.com>
2021-01-04 12:33:50 -0300

    Produccion

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("", include("projects.urls")),
 ]
 
-# Heroku
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# # Heroku
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
